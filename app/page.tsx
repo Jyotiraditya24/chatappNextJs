cb91550fe93a06635d091d65320df6ba8fbe5e7f
jyotiraditya <jyotiraditya24@gmail.com>
2023-05-24 15:40:52 +0530

    Redis database intergrated

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import { redis } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  redis.set("key", 10);
+  redis.set("key2",20);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     HELLO
+      HELLO
     </main>
-  )
+  );
 }
